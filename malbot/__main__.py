bda5d86d6c2a62e475da6918d307f2b19e329731
Dániel Boros <daniel.boros.17@gmail.com>
2021-08-21 16:13:24 +0200

    Init RCON client connection and add player list command

--- malbot/__main__.py
+++ malbot/__main__.py
@@ -4,6 +4,9 @@ import discord
 from discord.ext import commands
 from discord_slash import SlashCommand
 from malbot.log import init_logger
+from malbot.steam_api.server_info import RCON
+
+API_ENDPOINT = 'https://beguid-converter.allianceapps.io/v3'
 
 
 def main():
@@ -16,19 +19,29 @@ def main():
     slash = SlashCommand(client, sync_commands=True)
     guild_id = [int(os.environ['DISCORD_GUILD_ID'])]
 
+    # Set up RCON
+    rcon_client = RCON(api=API_ENDPOINT)
+
     @client.event
     async def on_ready():
         print('Logged in as {0.user}'.format(client))
-        logger.info('Clarkson is listening')
+        logger.info('Clarkson is alive')
 
-    @client.command()
-    async def ping(ctx):
-        """Checks for a response from the bot to see if it's alive."""
-        await ctx.send('Pong')
-
-    @slash.slash(name='Ping', description='Show the latency of the bot', guild_ids=guild_id)
+    @slash.slash(
+        name='Ping',
+        description='Show the latency of the bot',
+        guild_ids=guild_id
+    )
     async def ping(ctx):
         await ctx.send(f'Latency: {round(client.latency * 1000)}ms')
+
+    @slash.slash(
+        name='PlayerList',
+        description='Get the number of online players on the DayZ server',
+        guild_ids=guild_id
+    )
+    async def player_list(ctx):
+        await ctx.send(f'Online players: {await rcon_client.get_player_list()}')
 
     client.run(os.environ['DISCORD_TOKEN'])
 
---
+++ malbot/steam_api/server_info.py
@@ -0,0 +1,94 @@
+import os
+import bec_rcon
+import requests
+
+from steam.webapi import WebAPI
+from malbot.log import init_logger
+
+
+class RCON:
+    def __init__(self, api):
+        self.api_endpoint = api
+        self.logger = init_logger()
+
+    async def get_player_list(self):
+        player_steam_ids = await self.__convert_guids(await self.__get_players())
+        players = await self.__get_all_names_by_steam_ids(player_steam_ids)
+
+        return players
+
+    async def __rcon_connect(self):
+        try:
+            rcon_client = bec_rcon.ARC(os.environ['RCON_IP'], os.environ['RCON_PASSWORD'], int(os.environ['RCON_PORT']))
+            rcon_client.add_Event('received_ServerMessage', self.__handle_message)
+
+            self.logger.info('RCON connection successful')
+
+            return rcon_client
+        except Exception as e:
+            self.logger.error('RCON connection failed: ', e)
+
+            return None
+
+    async def __get_players(self):
+        try:
+            rcon_client = await self.__rcon_connect()
+
+            return await rcon_client.getPlayersArray()
+        except Exception as e:
+            self.logger.error('Getting player list failed: ', e)
+
+            return []
+        finally:
+            rcon_client.disconnect()
+
+            self.logger.info('RCON client disconnected')
+
+    async def __convert_guids(self, all_player_info):
+        try:
+            guids = '['
+
+            for player_info in all_player_info:
+                guids += '"{}",'.format(player_info[3])
+
+            guids = guids.rstrip(guids[-1])
+            guids += ']'
+
+            response = requests.post(url=self.api_endpoint, data=guids)
+            json = response.json()
+        except Exception as e:
+            self.logger.error('Converting GUID\'s failed: ', e)
+            return None
+
+        steam_ids = []
+
+        for guid in json['data']:
+            steam_ids.append(json['data'][guid])
+
+        return steam_ids
+
+    async def __get_name_by_steam_id(self, steam_id):
+        try:
+            api = WebAPI(key=os.environ['STEAM_API_KEY'])
+            api.ISteamUser.GetPlayerSummaries(steamids=steam_id)
+            response = api.call('ISteamUser.GetPlayerSummaries', steamids=steam_id)
+        except Exception as e:
+            self.logger.error('Getting Steam profile name failed: ', e)
+            return None
+
+        return response['response']['players'][0]['personaname']
+
+    async def __get_all_names_by_steam_ids(self, steam_ids):
+        profile_names = []
+
+        for current_id in steam_ids:
+            profile_names.append(await self.__get_name_by_steam_id(current_id))
+
+        return profile_names
+
+    def __handle_message(self, args):
+        message = args[0]
+        print(message)
+        self.logger.info(message)
+
+        return None
