e8379905ad02ac988a4d9ac0641140b0b1ad8519
Dániel Boros <daniel.boros.17@gmail.com>
2021-08-23 20:28:54 +0200

    Monkey patch log directory

--- malbot/log.py
+++ malbot/log.py
@@ -7,23 +7,25 @@ def init_logger():
     logger = logging.getLogger('discord')
     logger.setLevel(logging.INFO)
 
-    parent_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-    log_path = os.path.join(parent_path, 'logs')
+    # parent_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+    # log_path = os.path.join(parent_path, 'logs')
+    #
+    # if not os.path.isdir(log_path):
+    #     try:
+    #         os.mkdir(log_path)
+    #         print('Log directory created: ', parent_path)
+    #     except OSError as e:
+    #         print('Cannot create log directory: ', e)
+    #
+    # if not os.path.isfile(os.path.join(log_path, 'discord.log')):
+    #     try:
+    #         Path(os.path.join(log_path, 'discord.log')).touch()
+    #     except Exception as e:
+    #         print('Cannot create log file: ', e)
 
-    if not os.path.isdir(log_path):
-        try:
-            os.mkdir(log_path)
-            print('Log directory created: ', parent_path)
-        except OSError as e:
-            print('Cannot create log directory: ', e)
+    Path(os.path.join('/app/logs/discord.log')).touch()
 
-    if not os.path.isfile(os.path.join(log_path, 'discord.log')):
-        try:
-            Path(os.path.join(log_path, 'discord.log')).touch()
-        except Exception as e:
-            print('Cannot create log file: ', e)
-
-    handler = logging.FileHandler(filename=log_path + '/discord.log', encoding='utf-8', mode='w')
+    handler = logging.FileHandler(filename='/app/logs/discord.log', encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
 
